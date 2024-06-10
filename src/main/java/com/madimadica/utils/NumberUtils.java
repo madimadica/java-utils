d34c5d66281f52934e26c4c4b2fd237dc632b81b
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-06-09 19:40:51 -0500

    feat(int length): Add NumberUtils#length(long)

--- src/main/java/com/madimadica/utils/NumberUtils.java
+++ src/main/java/com/madimadica/utils/NumberUtils.java
@@ -3,8 +3,8 @@ package com.madimadica.utils;
 public class NumberUtils {
 
     /**
-     * Computes what the length of the base 10 {@link String} representation
-     * for the input value would be.
+     * Computes the length of the base 10
+     * {@link String} representation for a 32-bit {@code int}.
      * @param x number to compute the length of.
      * @return length of the number.
      */
@@ -56,4 +56,96 @@ public class NumberUtils {
         }
 
     }
+
+    /**
+     * Computes the length of the base 10
+     * {@link String} representation for a 64-bit {@code long}.
+     * @param x number to compute the length of.
+     * @return length of the number.
+     */
+    public static int length(long x) {
+        if (x < 0L) {
+            if (x > -10L) {
+                return 2;
+            } else if (x > -100L) {
+                return 3;
+            } else if (x > -1_000L) {
+                return 4;
+            } else if (x > -10_000L) {
+                return 5;
+            } else if (x > -100_000L) {
+                return 6;
+            } else if (x > -1_000_000L) {
+                return 7;
+            } else if (x > -10_000_000L) {
+                return 8;
+            } else if (x > -100_000_000L) {
+                return 9;
+            } else if (x > -1_000_000_000L) {
+                return 10;
+            } else if (x > -10_000_000_000L) {
+                return 11;
+            } else if (x > -100_000_000_000L) {
+                return 12;
+            } else if (x > -1_000_000_000_000L) {
+                return 13;
+            } else if (x > -10_000_000_000_000L) {
+                return 14;
+            } else if (x > -100_000_000_000_000L) {
+                return 15;
+            } else if (x > -1_000_000_000_000_000L) {
+                return 16;
+            } else if (x > -10_000_000_000_000_000L) {
+                return 17;
+            } else if (x > -100_000_000_000_000_000L) {
+                return 18;
+            } else if (x > -1_000_000_000_000_000_000L) {
+                return 19;
+            } else {
+                return 20;
+            }
+        } else {
+            if (x < 10L) {
+                return 1;
+            } else if (x < 100L) {
+                return 2;
+            } else if (x < 1_000L) {
+                return 3;
+            } else if (x < 10_000L) {
+                return 4;
+            } else if (x < 100_000L) {
+                return 5;
+            } else if (x < 1_000_000L) {
+                return 6;
+            } else if (x < 10_000_000L) {
+                return 7;
+            } else if (x < 100_000_000L) {
+                return 8;
+            } else if (x < 1_000_000_000L) {
+                return 9;
+            } else if (x < 10_000_000_000L) {
+                return 10;
+            } else if (x < 100_000_000_000L) {
+                return 11;
+            } else if (x < 1_000_000_000_000L) {
+                return 12;
+            } else if (x < 10_000_000_000_000L) {
+                return 13;
+            } else if (x < 100_000_000_000_000L) {
+                return 14;
+            } else if (x < 1_000_000_000_000_000L) {
+                return 15;
+            } else if (x < 10_000_000_000_000_000L) {
+                return 16;
+            } else if (x < 100_000_000_000_000_000L) {
+                return 17;
+            } else if (x < 1_000_000_000_000_000_000L) {
+                return 18;
+            } else {
+                return 19;
+            }
+        }
+
+    }
+
 }
--- src/test/java/com/madimadica/utils/NumberUtilsTests.java
+++ src/test/java/com/madimadica/utils/NumberUtilsTests.java
@@ -7,12 +7,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class NumberUtilsTests {
 
     @Test
-    void intLength() {
+    void intLengthFast() {
+        // Could test all but it takes roughly 40 seconds
+        for (int i = 1; i > 0; i *= 10) {
+            for (int offset = -10; offset < 10; ++offset) {
+                assertEquals(String.valueOf(i + offset).length(), NumberUtils.length(i + offset));
+                assertEquals(String.valueOf(-i + offset).length(), NumberUtils.length(-i + offset));
+            }
+        }
+        assertEquals(String.valueOf(Long.MIN_VALUE).length(), NumberUtils.length(Long.MIN_VALUE));
+        assertEquals(String.valueOf(Long.MAX_VALUE).length(), NumberUtils.length(Long.MAX_VALUE));
+    }
+
+    @Test
+    void intLengthEveryInput() {
         for (int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; ++i) {
             assertEquals(String.valueOf(i).length(), NumberUtils.length(i));
         }
-        assertEquals(String.valueOf(Integer.MAX_VALUE).length(), NumberUtils.length(Integer.MAX_VALUE));
+        assertEquals(String.valueOf(Long.MIN_VALUE).length(), NumberUtils.length(Long.MIN_VALUE));
+        assertEquals(String.valueOf(Long.MAX_VALUE).length(), NumberUtils.length(Long.MAX_VALUE));
     }
 
+    @Test
+    void longLengthFast() {
+        // Cannot test every number without waiting forever (~3 billion minutes)
+        // So just check that powers of 10 and surrounding numbers work
+        for (long i = 1; i > 0; i *= 10) {
+            for (int offset = -10; offset < 10; ++offset) {
+                assertEquals(String.valueOf(i + offset).length(), NumberUtils.length(i + offset));
+                assertEquals(String.valueOf(-i + offset).length(), NumberUtils.length(-i + offset));
+            }
+        }
+        assertEquals(String.valueOf(Long.MIN_VALUE).length(), NumberUtils.length(Long.MIN_VALUE));
+        assertEquals(String.valueOf(Long.MAX_VALUE).length(), NumberUtils.length(Long.MAX_VALUE));
+    }
 
 }
