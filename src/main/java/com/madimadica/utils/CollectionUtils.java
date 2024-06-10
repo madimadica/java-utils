2c2924f466d981d9631ac9b14309476363cf2ebc
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-06-09 23:49:47 -0500

    feat(CollectionUtils): Add initial collection utilities

---
+++ src/main/java/com/madimadica/utils/CollectionUtils.java
@@ -0,0 +1,97 @@
+package com.madimadica.utils;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public abstract class CollectionUtils {
+
+    /**
+     * Attempt to return the first element in the collection, according to its {@link Iterator}.
+     * Returns {@link Optional#empty()} if the input is empty,
+     * {@code null}, or the element itself is {@code null}.
+     * @param collection to get an element from.
+     * @return the first element, if present.
+     * @param <T> type of the elements in the Collection
+     */
+    public static <T> Optional<T> getFirst(Collection<T> collection) {
+        if (collection == null || collection.isEmpty()) {
+            return Optional.empty();
+        }
+        for (T t : collection) {
+            return Optional.ofNullable(t);
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Map an input collection to an output List.
+     * <br>
+     * Example: {@code List<Username> usernames = map(users, User::getUsername);}
+     * @param collection containing the elements to map.
+     * @param mappingFunction function remapping each element {@link X} to {@link Y}.
+     * @return the list mapped by the function.
+     * @param <X> type of the input {@code Collection}
+     * @param <Y> type of the output {@code List}
+     */
+    public static <X, Y> List<Y> map(Collection<X> collection, Function<? super X, ? extends Y> mappingFunction) {
+        return collection.stream().map(mappingFunction).collect(Collectors.toList());
+    }
+
+    /**
+     * Map an input Collection to an output Set<br>
+     * Example: {@code Set<String> uniqueLastNames = map(users, User::getLastName);}
+     * @param collection containing the elements to map.
+     * @param mappingFunction function remapping each element {@link X} to {@link Y}
+     * @return the Set of mapped results
+     * @param <X> type of the input {@code Collection}
+     * @param <Y> type of the output {@code Set}
+     */
+    public static <X, Y> Set<Y> mapToSet(List<X> collection, Function<? super X, ? extends Y> mappingFunction) {
+        return collection.stream().map(mappingFunction).collect(Collectors.toSet());
+    }
+
+    /**
+     * Create a {@code Map} from an input {@code Collection} where the Entries' values are elements from the input collection.
+     * <br>
+     * Collisions will cause an {@link IllegalStateException}
+     * <br>
+     * Example: {@code Map<Long, User> userLookup = asMap(users, User::getId);}
+     * @param collection Collection to create a Map from.
+     * @param keyMappingFunction Function to get the key for a given element.
+     * @return a Map with keys from {@code keyMappingFunction} and values of type {@link X}
+     * @param <X> type of input collection elements.
+     * @param <Y> type of output map keys.
+     */
+    public static <X, Y> Map<Y, X> asMap(Collection<X> collection, Function<? super X, ? extends Y> keyMappingFunction) {
+        return collection.stream().collect(Collectors.toMap(keyMappingFunction, Function.identity()));
+    }
+
+    /**
+     * Group an input collection by a mapped key into a Map of Lists by their key mappings.
+     * <br>
+     * Example {@code Map<String, List<User>> usersByLastName = groupBy(dataset, User::getLastName);}
+     * @param collection input collection
+     * @param groupClassifier Function to determine the correct group for each element.
+     * @return Map where each entry has the group classifier and list of elements in the group.
+     * @param <X> Type of input elements
+     * @param <Y> Type of classifier
+     */
+    public static <X, Y> Map<Y, List<X>> groupBy(Collection<X> collection, Function<? super X, ? extends Y> groupClassifier) {
+        return collection.stream().collect(Collectors.groupingBy(groupClassifier));
+    }
+
+    /**
+     * Convert an {@link Iterable} into a {@link List}
+     * @param iterable to convert
+     * @return List of the iterable elements
+     * @param <T> type of the elements
+     */
+    public static <T> List<T> toList(Iterable<T> iterable) {
+        List<T> results = new ArrayList<>();
+        for (T t : iterable) {
+            results.add(t);
+        }
+        return results;
+    }
+}
---
+++ src/test/java/com/madimadica/utils/CollectionUtilsTests.java
@@ -0,0 +1,150 @@
+package com.madimadica.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CollectionUtilsTests {
+
+    private static class Person {
+        Long id;
+        String firstName;
+        String lastName;
+
+        public Person(Long id, String firstName, String lastName) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getId(), getFirstName(), getLastName());
+        }
+    }
+
+    @Test
+    void getFirst_NullArgument() {
+        assertTrue(CollectionUtils.getFirst(null).isEmpty());
+    }
+
+    @Test
+    void getFirst_NullElement() {
+        List<Integer> testInput = new ArrayList<>();
+        testInput.add(null);
+        assertTrue(CollectionUtils.getFirst(testInput).isEmpty());
+    }
+
+    @Test
+    void getFirst_HappyPath() {
+        List<String> testInput = new ArrayList<>();
+        testInput.add("foo");
+        testInput.add("bar");
+        assertEquals(CollectionUtils.getFirst(testInput).get(), "foo");
+    }
+
+    @Test
+    void map() {
+        List<Person> people = List.of(
+                new Person(1L, "John", "Smith"),
+                new Person(2L, "Bob", "Smith")
+        );
+        List<String> firstNames = CollectionUtils.map(people, Person::getFirstName);
+        assertEquals(firstNames.size(), 2);
+        assertEquals(firstNames.get(0), "John");
+        assertEquals(firstNames.get(1), "Bob");
+    }
+
+    @Test
+    void mapToSet_OneToOne() {
+        List<Person> people = List.of(
+                new Person(1L, "John", "Smith"),
+                new Person(2L, "Bob", "Smith")
+        );
+        Set<String> firstNames = CollectionUtils.mapToSet(people, Person::getFirstName);
+        assertEquals(firstNames.size(), 2);
+    }
+
+    @Test
+    void mapToSet_ManyToOne() {
+        List<Person> people = List.of(
+                new Person(1L, "John", "Smith"),
+                new Person(2L, "Bob", "Smith")
+        );
+        Set<String> firstNames = CollectionUtils.mapToSet(people, Person::getLastName);
+        assertEquals(firstNames.size(), 1);
+    }
+
+    @Test
+    void asMap() {
+        Person john = new Person(1L, "John", "Smith");
+        Person bob = new Person(2L, "Bob", "Smith");
+
+        List<Person> people = List.of(john, bob);
+        Map<Long, Person> personLookup = CollectionUtils.asMap(people, Person::getId);
+        assertEquals(personLookup.get(1L), john);
+        assertEquals(personLookup.get(2L), bob);
+    }
+
+    @Test
+    void asMap_Duplicates() {
+        Person john = new Person(1L, "John", "Smith");
+        Person bob = new Person(2L, "Bob", "Smith");
+        Person bob2 = new Person(2L, "Bob2", "Smith2");
+
+        List<Person> people = List.of(john, bob, bob2);
+        assertThrows(IllegalStateException.class, () -> CollectionUtils.asMap(people, Person::getId));
+    }
+
+    @Test
+    void groupBy() {
+        Person john = new Person(1L, "John", "Smith");
+        Person bob = new Person(2L, "Bob", "Smith");
+        Person bob2 = new Person(2L, "Tim", "Cook");
+
+        List<Person> people = List.of(john, bob, bob2);
+        Map<String, List<Person>> peopleByLastName = CollectionUtils.groupBy(people, Person::getLastName);
+        assertEquals(peopleByLastName.get("Smith").size(), 2);
+        assertEquals(peopleByLastName.get("Cook").size(), 1);
+        assertEquals(peopleByLastName.size(), 2);
+        assertEquals(peopleByLastName.get("Smith").get(0), john);
+        assertEquals(peopleByLastName.get("Smith").get(1), bob);
+        assertEquals(peopleByLastName.get("Cook").get(0), bob2);
+    }
+
+    @Test
+    void iterableToList() {
+        Person john = new Person(1L, "John", "Smith");
+        Person bob = new Person(2L, "Bob", "Smith");
+        Person bob2 = new Person(2L, "Tim", "Cook");
+
+        Set<Person> people = Set.of(john, bob, bob2);
+        List<Person> result = CollectionUtils.toList(people);
+        assertEquals(result.size(), 3);
+        for (Person p : result) {
+            assertTrue(people.contains(p));
+        }
+    }
+}
