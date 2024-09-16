51e81475c7a7389bd5616615b06ed0516828c534
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-09-16 00:05:27 -0500

    Lists: Remove overloaded ofNullable
    Not getting a lot of performance gain. Shorten tests

--- src/test/java/com/madimadica/utils/list/TestLists.java
+++ src/test/java/com/madimadica/utils/list/TestLists.java
@@ -41,55 +41,6 @@ public class TestLists {
     }
 
     @Test
-    void nullableList4() {
-        List<Integer> list = Lists.ofNullable(1, 2, 3, null);
-        assertEquals(4, list.size());
-        assertThrows(UnsupportedOperationException.class, () -> list.add(0));
-    }
-
-    @Test
-    void nullableList5() {
-        List<Integer> list = Lists.ofNullable(1, 2, 3, 4, null);
-        assertEquals(5, list.size());
-        assertThrows(UnsupportedOperationException.class, () -> list.add(0));
-    }
-
-    @Test
-    void nullableList6() {
-        List<Integer> list = Lists.ofNullable(1, 2, 3, 4, 5, null);
-        assertEquals(6, list.size());
-        assertThrows(UnsupportedOperationException.class, () -> list.add(0));
-    }
-
-    @Test
-    void nullableList7() {
-        List<Integer> list = Lists.ofNullable(1, 2, 3, 4, 5, 6, null);
-        assertEquals(7, list.size());
-        assertThrows(UnsupportedOperationException.class, () -> list.add(0));
-    }
-
-    @Test
-    void nullableList8() {
-        List<Integer> list = Lists.ofNullable(1, 2, 3, 4, 5, 6, 7, null);
-        assertEquals(8, list.size());
-        assertThrows(UnsupportedOperationException.class, () -> list.add(0));
-    }
-
-    @Test
-    void nullableList9() {
-        List<Integer> list = Lists.ofNullable(1, 2, 3, 4, 5, 6, 7, 8, null);
-        assertEquals(9, list.size());
-        assertThrows(UnsupportedOperationException.class, () -> list.add(0));
-    }
-
-    @Test
-    void nullableList10() {
-        List<Integer> list = Lists.ofNullable(1, 2, 3, 4, 5, 6, 7, 8, 9, null);
-        assertEquals(10, list.size());
-        assertThrows(UnsupportedOperationException.class, () -> list.add(0));
-    }
-
-    @Test
     void nullableListVarargs() {
         List<Integer> list = Lists.ofNullable(1, 2, 3, 4, 5, 6, 7, 8, 9, null, 11);
         assertEquals(11, list.size());
