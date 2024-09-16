44df7a773d7f02ed548b3e0dd1dc0f768fe8083b
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-09-15 22:45:49 -0500

    NumberUtils#length: Fix boundary tests
    Was comparing Long.MAX_VALUE and Long.MIN_VALUE instead of Integer.MAX_VALUE and Integer.MIN_VALUE. Added some brief comments to the test cases

--- src/test/java/com/madimadica/utils/NumberUtilsTests.java
+++ src/test/java/com/madimadica/utils/NumberUtilsTests.java
@@ -6,28 +6,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NumberUtilsTests {
 
+    /**
+     * Test a few values around each order of magnitude
+     * for {@link NumberUtils#length(int)}.
+     */
     @Test
     void intLengthFast() {
-        // Could test all but it takes roughly 40 seconds
         for (int i = 1; i > 0; i *= 10) {
             for (int offset = -10; offset < 10; ++offset) {
                 assertEquals(String.valueOf(i + offset).length(), NumberUtils.length(i + offset));
                 assertEquals(String.valueOf(-i + offset).length(), NumberUtils.length(-i + offset));
             }
         }
-        assertEquals(String.valueOf(Long.MIN_VALUE).length(), NumberUtils.length(Long.MIN_VALUE));
-        assertEquals(String.valueOf(Long.MAX_VALUE).length(), NumberUtils.length(Long.MAX_VALUE));
+        assertEquals(String.valueOf(Integer.MIN_VALUE).length(), NumberUtils.length(Integer.MIN_VALUE));
+        assertEquals(String.valueOf(Integer.MAX_VALUE).length(), NumberUtils.length(Integer.MAX_VALUE));
     }
 
-//    @Test
+    /**
+     * Test all possible <code>int</code> arguments
+     * for {@link NumberUtils#length(int)}.
+     */
+    @Test
     void intLengthEveryInput() {
-        for (int i = Integer.MIN_VALUE; i < Integer.MAX_VALUE; ++i) {
+        for (int i = Integer.MIN_VALUE + 1; i < Integer.MAX_VALUE; ++i) {
             assertEquals(String.valueOf(i).length(), NumberUtils.length(i));
         }
-        assertEquals(String.valueOf(Long.MIN_VALUE).length(), NumberUtils.length(Long.MIN_VALUE));
-        assertEquals(String.valueOf(Long.MAX_VALUE).length(), NumberUtils.length(Long.MAX_VALUE));
+        assertEquals(String.valueOf(Integer.MIN_VALUE).length(), NumberUtils.length(Integer.MIN_VALUE));
+        assertEquals(String.valueOf(Integer.MAX_VALUE).length(), NumberUtils.length(Integer.MAX_VALUE));
     }
 
+    /**
+     * Test a few values around each order of magnitude
+     * for {@link NumberUtils#length(long)}.
+     */
     @Test
     void longLengthFast() {
         // Cannot test every number without waiting forever (~3 billion minutes)
