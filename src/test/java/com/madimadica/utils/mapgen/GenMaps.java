0dff618f9a3558aeae66efce685e5a77307edfa0
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-09-22 19:07:16 -0500

    v1.1.0 Add mapgen for Maps.ofOrdered

--- src/test/java/com/madimadica/utils/mapgen/GenMaps.java
+++ src/test/java/com/madimadica/utils/mapgen/GenMaps.java
@@ -18,6 +18,7 @@ public class GenMaps {
         lines.add("");
         lines.add("import java.util.Collections;");
         lines.add("import java.util.HashMap;");
+        lines.add("import java.util.LinkedHashMap;");
         lines.add("import java.util.Map;");
         lines.add("");
         lines.add("/**");
@@ -31,19 +32,23 @@ public class GenMaps {
         lines.add("");
         lines.add(ofNullable());
         lines.add(ofMutable());
+        lines.add(ofOrdered());
         lines.add("");
         lines.add("}");
         lines.add("");
         Files.writeString(Paths.get("./src/main/java/com/madimadica/utils/Maps.java"), lines.toString());
     }
 
-    private static String assertNonNullKeys(int keyCount) {
+    private static String assertNonNullArgs(int keyCount, boolean allowNullValues) {
         StringJoiner nonNullIf = new StringJoiner(
                 " || ",
                 "        if (", ") {\n            throw new NullPointerException(\"Map keys cannot be null\");\n        }"
         );
         for (int i = 1; i <= keyCount; ++i) {
             nonNullIf.add("k" + i + " == null");
+            if (!allowNullValues) {
+                nonNullIf.add("v" + i + " == null");
+            }
         }
         return nonNullIf.toString();
     }
@@ -60,7 +65,7 @@ public class GenMaps {
                     "<br>",
                     "The values can be {@code null}, but keys must be non-null."
             );
-            result.add(generate(i, "ofNullable", "HashMap", "Collections.unmodifiableMap(map)", javadoc));
+            result.add(generate(i, "ofNullable", "HashMap", "Collections.unmodifiableMap(map)", javadoc, true));
         }
         return result.toString();
     }
@@ -73,12 +78,25 @@ public class GenMaps {
                     "<br>",
                     "The values can be {@code null}, but keys must be non-null."
             );
-            result.add(generate(i, "ofMutable", "HashMap", "map", javadoc));
+            result.add(generate(i, "ofMutable", "HashMap", "map", javadoc, true));
         }
         return result.toString();
     }
 
-    private static String generate(int entryCount, String methodName, String mapClass, String returnValue, List<String> javadocDesc) {
+    private static String ofOrdered() {
+        StringJoiner result = new StringJoiner("\n\n");
+        for (int i = 1; i <= MAX_ENTRIES; ++i) {
+            List<String> javadoc = List.of(
+                    "Creates an ordered, immutable map with " + i + " " + (i == 1 ? "entry" : "entries") + ".",
+                    "<br>",
+                    "Neither the keys nor values may be {@code null}."
+            );
+            result.add(generate(i, "ofOrdered", "LinkedHashMap", "Collections.unmodifiableMap(map)", javadoc, false));
+        }
+        return result.toString();
+    }
+
+    private static String generate(int entryCount, String methodName, String mapClass, String returnValue, List<String> javadocDesc, boolean allowNullValues) {
         StringJoiner javadoc = getJavadocJoiner();
         javadocDesc.forEach(javadoc::add);
 
@@ -89,7 +107,11 @@ public class GenMaps {
         javadoc.add("@return An immutable map with the given entries.");
         javadoc.add("@param <K> key type");
         javadoc.add("@param <V> key type");
-        javadoc.add("@throws NullPointerException if any key is {@code null}.");
+        if (allowNullValues) {
+            javadoc.add("@throws NullPointerException if any key is {@code null}.");
+        } else {
+            javadoc.add("@throws NullPointerException if any <em>key or value</em> is {@code null}.");
+        }
 
         StringJoiner signatureSj = new StringJoiner(", ", "public static <K, V> Map<K, V> " + methodName + "(", ")");
         for (int j = 1; j <= entryCount; ++j) {
@@ -98,7 +120,7 @@ public class GenMaps {
         }
         String signature = signatureSj.toString();
         StringJoiner methodSj = new StringJoiner("\n", javadoc + "\n    " + signature + " {\n", "\n    }");
-        methodSj.add(assertNonNullKeys(entryCount));
+        methodSj.add(assertNonNullArgs(entryCount, allowNullValues));
         methodSj.add("        Map<K, V> map = new " + mapClass + "<>(" + entryCount + ");");
         for (int entryNumber = 1; entryNumber <= entryCount; ++entryNumber) {
             methodSj.add(String.format("        map.put(k%d, v%<d);", entryNumber));
